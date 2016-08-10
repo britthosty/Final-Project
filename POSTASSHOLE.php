5ce4718429bccb5515f50cdcb2f25b11907029de
britthosty <britthosty@gmail.com>
2016-08-10 15:32:59 -0500

    Add files via upload

---
+++ POSTASSHOLE.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Blog Posts</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel= "stlyecss" href="blogcss.css" type="text/css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<script type="text/javascript" async="" src="//api.viglink.com/api/vglnk.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" defer=""></script>
+	<script language="JavaScript" type="text/javascript" src="forum1_global.js" defer=""></script>
+	<script language="JavaScript" type="text/javasript" src="java_for_blog.js" defer=""></script>
+	<script language="JavaScript" type="text/javasript" src="spellChecker.js" defer=""></script>
+	<link href="post.html" rel="comments" type="text/html">
+	<link href="content.css" rel="thing" type="text/css">
+	<link href="printadd.html" rel="comments" type="text/html">
+	
+	
+    <title>Forum</title>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+	<script language="JavaScript" type="text/javascript">
+	var global_hash = {AppId : '', perm_view_profiles : '1', forum_embed_url : '', date_format : '3', display_time : '0', uid : '206075', forum_SSO_login_url : '', less_variables : ''};
+	
+	var	isPostRequest = 0;
+	
+	</script><script type="text/javascript" charset="utf-8" src="https://ajax.googleapis.com/ajax/libs/dojo/1.4.1/dojo/io/script.xd.js"></script>
+	<link rel="stylesheet" type="text/css" href="blogcss.css">
+
+	
+	<style type="text/css" id="forum_tree-style"> .treeview .list-group-item{cursor:pointer}.treeview span.indent{margin-left:10px;margin-right:10px}.treeview span.icon{width:12px;margin-right:5px}.treeview .node-disabled{color:silver;cursor:not-allowed}.node-forum_tree{}.node-forum_tree:not(.node-disabled):hover{background-color:#F5F5F5;} </style>
+</head>
+<body>
+	<?php echo $_POST; ?><br>
+</body>
+</html>
